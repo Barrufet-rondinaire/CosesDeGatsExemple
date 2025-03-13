513cc1d3f1e1bfdbf9fb0737697e60c1289694ef
Xavier Sala <fsala2@cendrassos.net>
2025-03-13 17:00:16 +0100

    chore: creació del repositori

---
+++ web/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Http.Json;
+
+namespace web;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var url = "https://catfact.ninja";
+        Uri uri = new(url);
+
+        Console.WriteLine(uri.Host);
+        Console.WriteLine(uri.Port);
+        
+        using HttpClient client = new()
+        {
+            BaseAddress = uri
+        }; 
+        
+        var response = await client.GetAsync("fact");
+        
+        response.EnsureSuccessStatusCode();
+        
+        Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+        var cosesdegat = await client.GetFromJsonAsync<Gat>("fact");
+
+        if (cosesdegat != null) Console.WriteLine(cosesdegat.fact);
+    }
+}
+
+public class Gat
+{
+    public string fact { get; set; }
+    public int length { get; set; }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
